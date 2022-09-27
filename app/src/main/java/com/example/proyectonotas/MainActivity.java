214015bcdc7906b3254825a40db6ea6294a6a4e9
Nicolás <nicolasignacio931@gmail.com>
2022-09-27 14:47:11 -0300

    commit

--- app/src/main/java/com/example/proyectonotas/MainActivity.java
+++ app/src/main/java/com/example/proyectonotas/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.proyectonotas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
+    public void ClickTareas(View view) {
+        Intent i = new Intent(this, MainNota.class);
+        startActivity(i);
+    }
+
 }
