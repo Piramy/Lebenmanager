f4423534f5851c53828014447a92a21681e14611
Nicolás <nicolasignacio931@gmail.com>
2022-11-24 09:26:59 -0300

    commit

--- app/src/main/java/com/example/proyectonotas/DialogCloseListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyectonotas;
-
-import android.content.DialogInterface;
-
-public interface DialogCloseListener {
-    public void handleDialogClose(DialogInterface dialog);
-}
